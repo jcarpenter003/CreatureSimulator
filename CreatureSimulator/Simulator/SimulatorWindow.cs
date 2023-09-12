779e6295fb3dc2334a0fd2335de550f267823917
Jonathan <jonathanemwd@outlook.com>
2023-09-12 16:44:24 -0400

    Drawing 10x10 grid

--- CreatureSimulator/Simulator/SimulatorWindow.cs
+++ CreatureSimulator/Simulator/SimulatorWindow.cs
@@ -1,8 +1,4 @@
-﻿using System.Windows.Forms;
-using System.Windows;
-using System.Reflection.Metadata;
-
-namespace CreatureSimulator.Simulator
+﻿namespace CreatureSimulator.Simulator
 {
     public partial class SimulatorWindow : Form
     {
@@ -10,9 +6,12 @@ namespace CreatureSimulator.Simulator
 
         public SimulatorWindow()
         {
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+
+            this.FormClosing += new FormClosingEventHandler(SimulatorWindow_FormClosing);
+
             InitializeComponent();
             this.CenterToScreen();
-            this.FormClosing += new FormClosingEventHandler(SimulatorWindow_FormClosing);
         }
 
         private void InitializeComponent()
@@ -26,15 +25,38 @@ namespace CreatureSimulator.Simulator
             panel.BorderStyle = BorderStyle.Fixed3D;
             panel.Location = new Point(0, 0);
             panel.Name = "panel";
-            panel.Size = new Size(285, 264);
+            panel.Size = new Size(1000, 1000);
             panel.TabIndex = 0;
+            panel.Paint += PaintGrid;
             // 
             // SimulatorWindow
             // 
-            ClientSize = new Size(284, 261);
+            ClientSize = new Size(1230, 1059);
             Controls.Add(panel);
             Name = "SimulatorWindow";
             ResumeLayout(false);
+        }
+
+        private void PaintGrid(object? sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            var pen = new Pen(Color.Black, 1);
+
+            using (Graphics g = e.Graphics)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    // Draws Columns
+                    var point1 = new Point(i * 100, 0);
+                    var point2 = new Point(i * 100, 1000);
+                    g.DrawLine(pen, point1, point2);
+
+                    // Draws Rows
+                    var rowPoint1 = new Point(0, i * 100);
+                    var rowPoint2 = new Point(1000, i * 100);
+                    g.DrawLine(pen, rowPoint1, rowPoint2);
+                }
+            }
         }
 
         private void SimulatorWindow_FormClosing(Object sender, FormClosingEventArgs e)
